f68d469e97b70ead8489a11e09d4314272f9e448
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-06-28 14:39:35 -0300

    feat: Connecting to the database

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import app from "./app";
+import connectDB from './db/connect'
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
